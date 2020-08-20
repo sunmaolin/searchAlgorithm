3f7e99c86b32745bf396fd5b6d833900cf61209b
sunmaolin <Mr.sun727@iCloud.com>
2020-08-20 23:14:08 +0800

    二分查找优化

--- src/sml/BinarySerach.java
+++ src/sml/BinarySerach.java
@@ -1,10 +1,21 @@
 package sml;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 二分查找（折半查找）
  * 数组必须是有序的
  */
 public class BinarySerach {
+    /**
+     * 只能查找出第一个值（普通版）
+     * @param array 需要查找的数组
+     * @param left 左边下标
+     * @param right 右边下标
+     * @param findValue 查找的值
+     * @return
+     */
     public static int binarySearch(int[] array,int left,int right,int findValue){
         //没有找到的情况就是
         if(left > right){
@@ -18,6 +29,45 @@ public class BinarySerach {
             return binarySearch(array,left,midIndex-1,findValue);
         }else{
             return midIndex;
+        }
+    }
+
+    /**
+     * 上一个方法的优化，查找所有相同值的下标
+     * @param array
+     * @param left
+     * @param right
+     * @param findValue
+     * @return
+     */
+    public static List<Integer> binarySearch2(int[] array,int left,int right,int findValue){
+        if(left > right){
+            return new ArrayList<>();
+        }
+        int mid = (left + right)/2;
+        if(findValue > array[mid]){
+            return binarySearch2(array,mid+1,right,findValue);
+        }else if (findValue < array[mid]){
+            return binarySearch2(array,left,mid-1,findValue);
+        }else{
+            List<Integer> indexList = new ArrayList<>();
+            indexList.add(mid);//先将第一次查找的数放进数组中
+            //因为是有序数组，往左边，右边查相同的数，添加进下标集合
+            int temp = mid + 1;
+            while(true){
+                if(temp > right || array[temp] != findValue){
+                    break;
+                }
+                indexList.add(temp++);
+            }
+            temp = mid - 1;
+            while (true){
+                if(temp < left || array[temp] != findValue){
+                    break;
+                }
+                indexList.add(temp--);
+            }
+            return indexList;
         }
     }
 }
--- src/sml/SearchTest.java
+++ src/sml/SearchTest.java
@@ -1,13 +1,17 @@
 package sml;
 
+import java.util.List;
+
 public class SearchTest {
     public static void main(String[] args) {
-        int[] array = {0,1,2,3,4,5,6,7};
+        int[] array = {0,0,0,0,1,2,3,4,5,6,7};
         int index;
         //测试线性查找
 //        index = SeqSearch.seqSearch(array,0);
         //测试基础二分查找
-        index = BinarySerach.binarySearch(array,0,array.length-1,-1);
-        System.out.println(index);
+//        index = BinarySerach.binarySearch(array,0,array.length-1,-1);
+        //测试优化二分查找，查找所有的相同值的下标
+        List<Integer> indexList = BinarySerach.binarySearch2(array,0,array.length-1,0);
+        System.out.println(indexList.size());
     }
 }
