da9a7816e16d994cdf6ba0c4eed9a3ec4275cca9
sunmaolin <Mr.sun727@iCloud.com>
2020-09-12 00:26:07 +0800

    dfs 深度优先遍历

--- src/sml/graph/Graph.java
+++ src/sml/graph/Graph.java
@@ -2,11 +2,16 @@ package sml.graph;
 
 import java.util.ArrayList;
 
+/**
+ * 图
+ */
 public class Graph {
 
     private ArrayList<String> vertexList;//存储顶点集合
     private int[][] edges;//存储图对应的邻接矩阵
     private int numOfEdges;//表示边的数目
+    //定义一个数组boolean[] 记录某个节点是否被访问
+    private boolean[] isVisited;
 
     public static void main(String[] args) {
         //测试图是否创建成功
@@ -24,13 +29,72 @@ public class Graph {
         graph.insertEdge(1,3,1);
         graph.insertEdge(1,4,1);
         graph.showGraph();
+
+        //测试深度优先遍历
+        graph.dfs();
     }
 
     public Graph(int vertexLength) {
         //初始化矩阵和vertexList
         edges = new int[vertexLength][vertexLength];
+        isVisited = new boolean[vertexLength];
         vertexList = new ArrayList<String>(vertexLength);
         numOfEdges = 0;//默认
+    }
+
+    /**
+     * 得到第一个邻接节点的下标w
+     * @param index
+     * @return 如果存在就返回对应的下标，否则返回-1
+     */
+    public int getFirstNeighbor(int index){
+        for (int i = 0; i < vertexList.size(); i++) {
+            if(edges[index][i] > 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 根据前一个邻接节点的下标来获取下一个临界节点
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public int getNextNeighbor(int v1,int v2){
+        for (int i = v2 + 1; i<vertexList.size(); i++){
+            if(edges[v1][i] > 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //深度优先遍历算法
+    //i 第一次就是0
+    private void dfs(boolean[] isVisited,int i){
+        //首先访问该节点，输出
+        System.out.print(getValueByIndex(i) + "->");
+        //将该节点设置为已经访问过
+        isVisited[i] = true;
+        //查找节点i的第一个邻接点w
+        int w = getFirstNeighbor(i);
+        while(w != -1){
+            if(!isVisited[w]){
+                dfs(isVisited,w);
+            }
+            //如果w节点已经被访问过
+            w = getNextNeighbor(i,w);
+        }
+    }
+
+    public void dfs(){
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if(!isVisited[i]){
+                dfs(isVisited,i);
+            }
+        }
     }
 
     //图中常用的方法
